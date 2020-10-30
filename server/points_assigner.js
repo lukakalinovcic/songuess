03b1faf4c242d62b5bef89bc9768d876388b5148
Tomislav Grbin <tomislav.grbin@gmail.com>
2020-10-30 16:06:58 +0100

    Adding more points_assigner tests and functionality.

--- server/points_assigner.js
+++ server/points_assigner.js
@@ -9,33 +9,88 @@ module.exports = function (currentItem, chatRoom) {
     answerChecker = new AnswerChecker(),
     maxPoints = (chatRoom.desc && chatRoom.desc.maxPoints) || 1,
     artistPoints = (chatRoom.desc && chatRoom.desc.artistPoints) || false,
-    remainingPoints = maxPoints,
-    titleWinners = new Set(),
-    artistWinners = new Set();
+    playOn = false,
+    // These arrays contain client objects.
+    // They are ordered, clients that come earlier should get more points.
+    titleWinners = [],
+    artistWinners = [];
+
+  function clientPresent(arr, client) {
+    for (const c of arr) {
+      if (c.id() == client.id()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Assumes the client with given ID exists!
+  function removeClient(arr, client) {
+    let j = 0;
+    for (let i = 0; i < arr.length; ++i) {
+      arr[j] = arr[i];
+      if (arr[i].id() != client.id()) {
+        ++j;
+      }
+    }
+    arr.pop();
+  }
 
   this.gotAnswer = function (answerData, client) {
-    if (answerChecker.checkAnswer(currentItem.title, answerData.what) &&
-        !titleWinners.has(client.id())) {
-      chatRoom.grantScore(client, remainingPoints);
-      chatRoom.broadcast('correct_title', {
-        who: client.id(),
-        when : answerData.when,
-        numPoints: remainingPoints
-      });
+    const gotTitle = answerChecker.checkAnswer(currentItem.title, answerData.what);
+    const gotArtist = artistPoints && answerChecker.checkAnswer(currentItem.artist, answerData.what);
 
-      remainingPoints--;
-      titleWinners.add(client.id());
-
-      if (remainingPoints === 0) {  // or no more possible clients to answer
-        const playOn = (answerData.what.indexOf('#playon') != -1);
-        chatRoom.guessingDone(playOn);
-      }
-    } else if (answerChecker.checkAnswer(currentItem.artist, answerData.what)) {
-      chatRoom.broadcast('correct_artist', {
+    if (gotTitle || gotArtist) {
+      playOn = playOn || (answerData.what.indexOf('#playon') != -1);
+      chatRoom.broadcast(gotTitle? 'correct_title': 'correct_artist', {
         who: client.id(),
         when : answerData.when
       });
-      artistWinners.add(client.id());
     }
+
+    if (gotTitle) {
+      if (!clientPresent(titleWinners, client)) {
+        // This is the case when a user first gets an artist right, but then also
+        // gets the title. In this case we remove from artistWinners and append
+        // to titleWinners.
+        if (clientPresent(artistWinners, client)) {
+          removeClient(artistWinners, client);
+        }
+        titleWinners.push(client);
+        // Title points can't change later and can be awarded immediately.
+        chatRoom.grantScore(client, maxPoints - titleWinners.length + 1);
+      }
+    } else if (gotArtist) {
+      if (!clientPresent(titleWinners, client) && !clientPresent(artistWinners, client)) {
+        // Artist points could later become title points so they are not
+        // awarded right away.
+        artistWinners.push(client);
+      }
+    }
+
+    // TODO: The current number of clients is also important here, for example
+    // if maxPoints = 4 but there are already 2 clients and they all gave their
+    // title answers already.
+    // If everyone got title points, we can close the round.
+    if (titleWinners.length == maxPoints ||
+        // If just one title point is left unawarded, and someone has an artist
+        // point, the situation also can't change anymore.
+        (artistWinners.length == 1 && titleWinners.length == maxPoints - 1)) {
+      // Not that if there are 2 artist winners, the one which would've gotten
+      // 1 point can still give a title answer and by doing that get 2 points
+      // instead of 1, so we can't finish the round.
+      that.finishTheRound();
+    }
+  };
+
+  // This is called if the song has ended, or if next was called before all the
+  // points were given out.
+  this.finishTheRound = function () {
+    // Title points have already been awarded.
+    let points = maxPoints - titleWinners.length;
+    for (const client of artistWinners) {
+      chatRoom.grantScore(client, points--);
+    }
+    chatRoom.guessingDone(playOn);
   };
 };
--- server/points_assigner.test.js
+++ server/points_assigner.test.js
@@ -7,8 +7,7 @@ var
   ARTIST = 'some artist',
   ITEM = {title: TITLE, artist: ARTIST},
   CLIENT_1 = {id: () => "1"},
-  CLIENT_2 = {id: () => "2"},
-  CLIENT_3 = {id: () => "3"};
+  CLIENT_2 = {id: () => "2"};
 
 function buildChatRoom(desc) {
   return {
@@ -20,20 +19,22 @@ function buildChatRoom(desc) {
 }
 
 test('maxPoints = 1, wrong answer', () => {
-  var chatRoom = buildChatRoom({maxPoints: 1});
-  var pa = new PointsAssigner(ITEM, chatRoom);
+  let chatRoom = buildChatRoom({maxPoints: 1});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
 
-  pa.gotAnswer({what: 'wrong', when: 1}, CLIENT_1);
+  pa.gotAnswer({what: 'wrong', when: when++}, CLIENT_1);
 
   expect(chatRoom.grantScore).not.toBeCalled();
   expect(chatRoom.guessingDone).not.toBeCalled();
 });
 
 test('maxPoints = 1, correct answer', () => {
-  var chatRoom = buildChatRoom({maxPoints: 1});
-  var pa = new PointsAssigner(ITEM, chatRoom);
+  let chatRoom = buildChatRoom({maxPoints: 1});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
 
-  pa.gotAnswer({what: TITLE, when: 1}, CLIENT_1);
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_1);
 
   expect(chatRoom.grantScore).toBeCalledTimes(1);
   expect(chatRoom.grantScore).toBeCalledWith(CLIENT_1, 1);
@@ -41,66 +42,233 @@ test('maxPoints = 1, correct answer', () => {
   expect(chatRoom.guessingDone).toBeCalledWith(/*playon=*/false);
 });
 
-test('maxPoints = 2, one correct one wrong answer', () => {
-  var chatRoom = buildChatRoom({maxPoints: 2});
-  var pa = new PointsAssigner(ITEM, chatRoom);
+test('maxPoints = 2, guessing not done, title point granted', () => {
+  let chatRoom = buildChatRoom({maxPoints: 2});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
 
-  pa.gotAnswer({what: TITLE, when: 1}, CLIENT_1);
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_1);
+  pa.gotAnswer({what: 'wrong', when: when++}, CLIENT_2);
+
   expect(chatRoom.grantScore).toBeCalledTimes(1);
-  // First correct answer gets two points.
   expect(chatRoom.grantScore).toBeCalledWith(CLIENT_1, 2);
-
-  pa.gotAnswer({what: 'wrong', when: 2}, CLIENT_2);
   expect(chatRoom.guessingDone).not.toBeCalled();
 });
 
 test('maxPoints = 2, various answers but finally two correct ones', () => {
-  var chatRoom = buildChatRoom({maxPoints: 2});
-  var pa = new PointsAssigner(ITEM, chatRoom);
+  let chatRoom = buildChatRoom({maxPoints: 2});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
 
-  pa.gotAnswer({what: 'wrong', when: 1}, CLIENT_1);
-  pa.gotAnswer({what: 'wrong', when: 2}, CLIENT_2);
-  pa.gotAnswer({what: TITLE, when: 3}, CLIENT_1);
+  pa.gotAnswer({what: 'wrong', when: when++}, CLIENT_1);
+  pa.gotAnswer({what: 'wrong', when: when++}, CLIENT_2);
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_1);
   expect(chatRoom.grantScore).toBeCalledTimes(1);
-  expect(chatRoom.grantScore).toBeCalledWith(CLIENT_1, 2);
-  expect(chatRoom.guessingDone).not.toBeCalled();
+  expect(chatRoom.grantScore).nthCalledWith(1, CLIENT_1, 2);
 
-  pa.gotAnswer({what: 'wrong', when: 4}, CLIENT_1);
-  pa.gotAnswer({what: TITLE, when: 5}, CLIENT_2);
+  pa.gotAnswer({what: 'wrong', when: when++}, CLIENT_1);
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_2);
   expect(chatRoom.grantScore).toBeCalledTimes(2);
-  expect(chatRoom.grantScore).toBeCalledWith(CLIENT_2, 1);
+  expect(chatRoom.grantScore).nthCalledWith(2, CLIENT_2, 1);
 
   expect(chatRoom.guessingDone).toBeCalledTimes(1);
   expect(chatRoom.guessingDone).toBeCalledWith(/*playon=*/false);
 });
 
 test('maxPoints = 2, dont award to same client twice', () => {
-  var chatRoom = buildChatRoom({maxPoints: 2});
-  var pa = new PointsAssigner(ITEM, chatRoom);
+  let chatRoom = buildChatRoom({maxPoints: 2});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
 
-  pa.gotAnswer({what: TITLE, when: 1}, CLIENT_1);
-  pa.gotAnswer({what: TITLE, when: 2}, CLIENT_1);
-
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_1);
   expect(chatRoom.grantScore).toBeCalledTimes(1);
   expect(chatRoom.grantScore).toBeCalledWith(CLIENT_1, 2);
+  // This client already got the title point.
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_1);
+  expect(chatRoom.grantScore).toBeCalledTimes(1);
+
+  // Guessing is not done, still 1 more point to give out.
   expect(chatRoom.guessingDone).not.toBeCalled();
 });
 
 test('maxPoints = 3, 3 correct guesses get 3,2,1 points', () => {
-  var chatRoom = buildChatRoom({maxPoints: 3});
-  var pa = new PointsAssigner(ITEM, chatRoom);
+  const CLIENT_3 = {id: () => "3"};
+  let chatRoom = buildChatRoom({maxPoints: 3});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
 
-  pa.gotAnswer({what: TITLE, when: 1}, CLIENT_1);
-  expect(chatRoom.guessingDone).not.toBeCalled();
-  pa.gotAnswer({what: TITLE, when: 2}, CLIENT_2);
-  expect(chatRoom.guessingDone).not.toBeCalled();
-  pa.gotAnswer({what: TITLE, when: 3}, CLIENT_3);
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_1);
+  expect(chatRoom.grantScore).toBeCalledTimes(1);
+  expect(chatRoom.grantScore).nthCalledWith(1, CLIENT_1, 3);
 
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_2);
+  expect(chatRoom.grantScore).toBeCalledTimes(2);
+  expect(chatRoom.grantScore).nthCalledWith(2, CLIENT_2, 2);
+  expect(chatRoom.guessingDone).not.toBeCalled();
+
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_3);
   expect(chatRoom.grantScore).toBeCalledTimes(3);
-  expect(chatRoom.grantScore).toBeCalledWith(CLIENT_1, 3);
-  expect(chatRoom.grantScore).toBeCalledWith(CLIENT_2, 2);
-  expect(chatRoom.grantScore).toBeCalledWith(CLIENT_3, 1);
+  expect(chatRoom.grantScore).nthCalledWith(3, CLIENT_3, 1);
 
+  expect(chatRoom.guessingDone).toBeCalledTimes(1);
+  expect(chatRoom.guessingDone).toBeCalledWith(/*playon=*/false);
+});
+
+test('maxPoints = 2, artistPoints, only titles guessed', () => {
+  let chatRoom = buildChatRoom({maxPoints: 2, artistPoints: true});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
+
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_1);
+  expect(chatRoom.grantScore).toBeCalledTimes(1);
+  expect(chatRoom.grantScore).nthCalledWith(1, CLIENT_1, 2);
+  expect(chatRoom.guessingDone).not.toBeCalled();
+
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_2);
+  expect(chatRoom.grantScore).toBeCalledTimes(2);
+  expect(chatRoom.grantScore).nthCalledWith(2, CLIENT_2, 1);
+
+  expect(chatRoom.guessingDone).toBeCalledTimes(1);
+  expect(chatRoom.guessingDone).toBeCalledWith(/*playon=*/false);
+});
+
+test('maxPoints = 2, one correct title one artist', () => {
+  let chatRoom = buildChatRoom({maxPoints: 2, artistPoints: true});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
+
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_1);
+  expect(chatRoom.grantScore).toBeCalledTimes(1);
+  expect(chatRoom.grantScore).nthCalledWith(1, CLIENT_1, 2);
+  expect(chatRoom.guessingDone).not.toBeCalled();
+
+  pa.gotAnswer({what: ARTIST, when: when++}, CLIENT_2);
+  expect(chatRoom.grantScore).toBeCalledTimes(2);
+  expect(chatRoom.grantScore).nthCalledWith(2, CLIENT_2, 1);
+
+  expect(chatRoom.guessingDone).toBeCalledTimes(1);
+  expect(chatRoom.guessingDone).toBeCalledWith(/*playon=*/false);
+});
+
+test('maxPoints = 3, artist points only', () => {
+  let chatRoom = buildChatRoom({maxPoints: 3, artistPoints: true});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
+
+  pa.gotAnswer({what: ARTIST, when: when++}, CLIENT_1);
+  expect(chatRoom.grantScore).not.toBeCalled();
+
+  pa.gotAnswer({what: ARTIST, when: when++}, CLIENT_2);
+  expect(chatRoom.grantScore).not.toBeCalled();
+
+  pa.finishTheRound();
+
+  expect(chatRoom.grantScore).toBeCalledTimes(2);
+  expect(chatRoom.grantScore).nthCalledWith(1, CLIENT_1, 3);
+  expect(chatRoom.grantScore).nthCalledWith(2, CLIENT_2, 2);
+  expect(chatRoom.guessingDone).toBeCalledTimes(1);
+  expect(chatRoom.guessingDone).toBeCalledWith(/*playon=*/false);
+});
+
+test('maxPoints = 3, artist points and a title point', () => {
+  let chatRoom = buildChatRoom({maxPoints: 3, artistPoints: true});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
+
+  pa.gotAnswer({what: ARTIST, when: when++}, CLIENT_1);
+  expect(chatRoom.grantScore).not.toBeCalled();
+
+  // Title points awarded immediately.
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_2);
+  expect(chatRoom.grantScore).toBeCalledTimes(1);
+  expect(chatRoom.grantScore).nthCalledWith(1, CLIENT_2, 3);
+  expect(chatRoom.guessingDone).not.toBeCalled();
+
+  pa.finishTheRound();
+
+  // CLIENT_2 already got 3 title points, CLIENT_1 gets 2 artist points.
+  expect(chatRoom.grantScore).toBeCalledTimes(2);
+  expect(chatRoom.grantScore).nthCalledWith(2, CLIENT_1, 2);
+  expect(chatRoom.guessingDone).toBeCalledTimes(1);
+  expect(chatRoom.guessingDone).toBeCalledWith(/*playon=*/false);
+});
+
+test('maxPoints = 2, artist point ignored if you already have a title point', () => {
+  let chatRoom = buildChatRoom({maxPoints: 2, artistPoints: true});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
+
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_1);
+  expect(chatRoom.grantScore).toBeCalledTimes(1);
+  expect(chatRoom.grantScore).nthCalledWith(1, CLIENT_1, 2);
+
+  pa.gotAnswer({what: ARTIST, when: when++}, CLIENT_1);
+
+  pa.finishTheRound();
+
+  expect(chatRoom.grantScore).toBeCalledTimes(1);
+  expect(chatRoom.guessingDone).toBeCalledTimes(1);
+  expect(chatRoom.guessingDone).toBeCalledWith(/*playon=*/false);
+});
+
+test('maxPoints = 4, artist point becomes a title point', () => {
+  let chatRoom = buildChatRoom({maxPoints: 4, artistPoints: true});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
+
+  pa.gotAnswer({what: ARTIST, when: when++}, CLIENT_1);
+  expect(chatRoom.grantScore).not.toBeCalled();
+
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_2);
+  expect(chatRoom.grantScore).toBeCalledTimes(1);
+  expect(chatRoom.grantScore).nthCalledWith(1, CLIENT_2, 4);
+
+  // CLIENT_1 gets a title after they got the artist.
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_1);
+  expect(chatRoom.grantScore).toBeCalledTimes(2);
+  expect(chatRoom.grantScore).nthCalledWith(2, CLIENT_1, 3);
+
+  pa.finishTheRound();
+
+  // No more artist points awarded, the one we had already transitioned to a
+  // title point.
+  expect(chatRoom.grantScore).toBeCalledTimes(2);
+  expect(chatRoom.guessingDone).toBeCalledTimes(1);
+  expect(chatRoom.guessingDone).toBeCalledWith(/*playon=*/false);
+});
+
+test('maxPoints = 5, big case', () => {
+  const CLIENT_3 = {id: () => "3"};
+  const CLIENT_4 = {id: () => "4"};
+  const CLIENT_5 = {id: () => "5"};
+
+  let chatRoom = buildChatRoom({maxPoints: 5, artistPoints: true});
+  let pa = new PointsAssigner(ITEM, chatRoom);
+  let when = 1;
+
+  // Three correct artists right away.
+  pa.gotAnswer({what: ARTIST, when: when++}, CLIENT_1);
+  pa.gotAnswer({what: ARTIST, when: when++}, CLIENT_2);
+  pa.gotAnswer({what: ARTIST, when: when++}, CLIENT_3);
+  expect(chatRoom.grantScore).not.toBeCalled();
+
+  // CLIENT_2 gets the title as well, getting 5 points.
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_2);
+  expect(chatRoom.grantScore).toBeCalledTimes(1);
+  expect(chatRoom.grantScore).nthCalledWith(1, CLIENT_2, 5);
+
+  // CLIENT_4 gets the title.
+  pa.gotAnswer({what: TITLE, when: when++}, CLIENT_4);
+  expect(chatRoom.grantScore).toBeCalledTimes(2);
+  expect(chatRoom.grantScore).nthCalledWith(2, CLIENT_4, 4);
+
+  expect(chatRoom.guessingDone).not.toBeCalled();
+  pa.finishTheRound();
+
+  // Remaining artist points awarded at this point.
+  expect(chatRoom.grantScore).toBeCalledTimes(4);
+  expect(chatRoom.grantScore).nthCalledWith(3, CLIENT_1, 3);
+  expect(chatRoom.grantScore).nthCalledWith(4, CLIENT_3, 2);
   expect(chatRoom.guessingDone).toBeCalledTimes(1);
   expect(chatRoom.guessingDone).toBeCalledWith(/*playon=*/false);
 });
